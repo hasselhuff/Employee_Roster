dda6f359c668c922c9ab82801dedc2eeffb2c10b
Tucker Huff <thuff2721@gmail.com>
2026-04-17 21:41:14 -0400

    Additional functionality for Module 2 Week 1 Homework assignment

--- Program.cs
+++ Program.cs
@@ -1,19 +1,72 @@
-﻿Console.WriteLine("Welcome to the employee roster console");
+﻿using System.Globalization;
+
 Console.WriteLine("-------------------------------------------");
-Console.Write("\nPlease enter the number of employees you wish to enter: ");
+Console.WriteLine("| Welcome to the employee roster console  |");
+Console.WriteLine("-------------------------------------------");
+Console.Write("Please enter the number of employees you wish to enter: ");
 int numberOfEmployees = int.Parse(Console.ReadLine());
 Console.WriteLine($"Initializing roster for {numberOfEmployees} employees...");
 string[] employeeRoster = new string[numberOfEmployees];
-for (int i = 0; i < numberOfEmployees; i++)
+Console.Clear();
+menuSelection();
+
+void menuSelection()
 {
-    Console.Write($"Enter the first name of employee {i}: ");
-    string name = Console.ReadLine();
-    employeeRoster[i] = name;
+    while (true)
+    {
+        Console.WriteLine("-------------------------");
+        Console.WriteLine("|    Menu Selection     |");
+        Console.WriteLine("-------------------------");
+        Console.WriteLine("1: Add an employee");
+        Console.WriteLine("2: Show all employees");
+        Console.WriteLine("Q: Exit");
+        Console.Write("\nPlease enter the corresponding number to the action you would like to perform or 'q' to exit: ");
+        string menuSelectionPrompt = Console.ReadLine();
+        switch (menuSelectionPrompt.Trim().ToLower())
+        {
+            case "1":
+                Console.Write("\nEnter first name: ");
+                string firstName = Console.ReadLine();
+                addEmployee(firstName);
+                break;
+            case "2":
+                showEmployees();
+                break;
+            case "q":
+                return;
+                break;
+            default:
+                Console.WriteLine("Incorrect input. Please use the corresponding number to the action (ex: '1' to add an employee) or 'q' to exit.");
+                break;
+        }
+    }
 }
-Array.Sort(employeeRoster);
-Console.WriteLine("-------------------------------------------");
-Console.WriteLine("Current employee roster:");
-foreach (string name in employeeRoster)
+
+void addEmployee(string firstName)
 {
-    Console.WriteLine(name);
+    for (int i = 0; i < numberOfEmployees; i++)
+    {
+        if(employeeRoster[i] == null)
+        {
+            employeeRoster[i] = firstName;
+            return;
+        }
+    }
+    if(employeeRoster.Contains(firstName) == false)
+    {
+        Console.WriteLine("!!!  ERROR  !!!");
+        Console.WriteLine($"Roster capacity is full with {numberOfEmployees} unable to add {firstName}");
+        return;
+    }
+}
+
+void showEmployees()
+{
+    Array.Sort(employeeRoster);
+    Console.WriteLine("-------------------------------------------");
+    Console.WriteLine("Current employee roster:");
+    foreach (string name in employeeRoster)
+    {
+        Console.WriteLine(name);
+    }
 }
